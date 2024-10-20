ea329e48053404eea2a4ad495770c23a99a127c4
Henry <henry@holopro.tech>
2024-10-19 22:05:30 -0400

    added nordtron

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,4 +4,4 @@ if (-not $(oh-my-posh --version)) {
     Write-Host "Oh My Posh not found on system, installing."
     winget install JanDeDobbeleer.OhMyPosh -s winget
 }
-oh-my-posh init pwsh --config "$profilePath/custom_jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$profilePath/nordtron.omp.json" | Invoke-Expression
