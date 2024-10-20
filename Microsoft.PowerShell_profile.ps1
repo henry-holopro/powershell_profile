e35004f534893b3166dcad7f553802f8ba121e26
Henry <henry@holopro.tech>
2024-10-19 21:41:39 -0400

    pre console_title_template

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,7 @@
+$profilePath = Split-Path -Path $PROFILE -Parent
+
 if (-not $(oh-my-posh --version)) {
     Write-Host "Oh My Posh not found on system, installing."
     winget install JanDeDobbeleer.OhMyPosh -s winget
 }
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/jandedobbeleer.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$profilePath/custom_jandedobbeleer.omp.json" | Invoke-Expression
